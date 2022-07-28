9ddbb1b9ecb96ff88457362fff95487cba98eedf
Daryl Norrell <cheyannewascom@gmail.com>
2022-07-27 21:15:57 -0700

    commit tree d7d0b28269b4434d331225c089aa6ded5784c644

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+input.addEventListener('click', addingEventListener);
